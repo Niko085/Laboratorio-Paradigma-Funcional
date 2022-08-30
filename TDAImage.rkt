cdcf3e61929e8cc2856d0de2f628a94d6e71b3b1
Niko085 <nicolas.gajardo.p@usach.cl>
2022-08-30 02:22:44 -0400

    TDA Image

---
+++ TDAImage.rkt
@@ -0,0 +1,34 @@
+#lang racket
+
+;-----------------------------------TDA Image------------------------------------------------------------------------
+
+;-----------------------------------REPRESENTACION--------------------------------------------------------------------
+
+; Este TDA representa imagenes con bitmaps, hexmaps o pixmaps, además, incluye información de profundidad
+; en cada pixel
+
+;-----------------------------------CONSTRUCTORES---------------------------------------------------------------------
+
+; Dominio: 
+; Recorrido: 
+; Descripcion: Crea 
+; Tipo de recursion: No se utiliza recursion
+(define (pixbit )
+  )
+
+; Dominio: 
+; Recorrido: 
+; Descripcion: Crea 
+; Tipo de recursion: No se utiliza recursion
+(define (pixrgb )
+  )
+
+; Dominio: 
+; Recorrido: 
+; Descripcion: Crea 
+; Tipo de recursion: No se utiliza recursion
+(define (pixhex )
+  )
+
+;-----------------------------------OTRAS FUNCIONES-------------------------------------------------------------------
+
